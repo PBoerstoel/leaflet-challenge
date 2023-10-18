be42f903c6ce599be9f004c8f1491af506a7fdd4
PBoerstoel <104413315+PBoerstoel@users.noreply.github.com>
2023-10-18 00:04:40 -0400

    Minus legend

---
+++ static/js/logic.js
@@ -0,0 +1,60 @@
+let queryurl= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+let quakes=[]
+let lat=[]
+let long=[]
+let depth=[]
+let mag=[]
+let popups=[]
+function colorf(depth) {
+    if (depth <= 10) {return("yellow")
+    } else if (10<depth<=50) {return("green")
+    } else if (50<depth<=100) {return("red")
+    } else if (depth>100) {return("black")}
+};
+d3.json(queryurl).then(function(data) {
+    for (let i =0; i<data.features.length;i++) {
+        lat.push(data.features[i].geometry.coordinates[0]);
+        long.push(data.features[i].geometry.coordinates[1]);
+        depth.push(data.features[i].geometry.coordinates[2]);
+        mag.push(data.features[i].properties.mag);
+        quakes.push(L.circle([long[i],lat[i]], {
+            color: "black",
+            fillColor: colorf(depth[i]),
+            fillOpacity:.75,
+            radius: Math.sqrt(mag[i])*10000
+        }));
+
+        quakes[i].bindPopup(`felt by ${data.features[i].properties.felt}, ${data.features[i].properties.place}, at ${new Date(data.features[i].properties.time)}`)
+    };
+
+    
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    console.log("hi");
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+    let quakers = L.layerGroup(quakes);
+    let baseMaps = {
+        "Street Map": street,
+        "Topological Map":topo
+    };
+    let overlayMaps = {
+        "earthquakes": quakers
+    }
+
+    console.log("hi");
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [street, quakers]
+    });
+
+   console.log("hi");
+    L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+    }).addTo(myMap);
+
+});
+
